f53e4278ef54786524136c81044d0b95af120fad
zhang-tang <2907578142@qq.com>
2022-09-05 18:35:51 +0800

    ru

--- BosonKit.ts
+++ BosonKit.ts
@@ -1,6 +1,3 @@
-
-
-
 //% weight=100 color=#0fbc11 icon="\uf0b2"
 //% groups="['Analog', 'Digital', 'Heartbeat', 'RGB LED Strip lights', 'Servo']"
 namespace BosonKit {
@@ -635,10 +632,10 @@ namespace BosonKit {
     //% endHue.min=0 endHue.max=360
     //% start.min=0 start.max=6 start.defl=0
     //% end.min=0 end.max=6 end.defl=4
-    //% block="%start to %end RGBs show gradient color from %startHue to %endHue"
+    //% block="RGB LED %start to %end show gradient color from %startHue to %endHue"
     //% inlineInputMode=inline
     export function  M011_00184_ledRainbow(start: number, end: number, startHue: number, endHue: number) {
-        
+   
         if ((end < start)){
             let num = end;
             end = start;
