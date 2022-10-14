c4df58af45dd29a48e9e3ef16f8f3fb64012d072
zhang-tang <80501060+zhang-tang@users.noreply.github.com>
2022-10-14 15:13:16 +0800

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,1 @@
 
-basic.forever(function () {
-    if (BosonKit.bos0003_rotation(DigitalPin.P0) == 1) {
-        BosonKit.bos0024_rotation(AnalogPin.P8, 1000)
-    } else {
-        BosonKit.bos0024_rotation(AnalogPin.P8, 0)
-    }
-})
